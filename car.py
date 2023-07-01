8321b9e69c879eecc981f1126725fdd06f26c3de
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-02 01:34:39 +0800

    [Car class] - create a constructor

---
+++ car.py
@@ -0,0 +1,10 @@
+# Kristine Joy Barrina  # BSCPE 1-5
+# Abstraction and Encapsulation
+# July 1, 2023
+
+class Car:
+    def __init__(self, year_model, make, speed):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = speed
+        
