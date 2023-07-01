4f7b35e7b9c67c04d4f1c5b26dfc4a1e5b353196
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-02 01:53:34 +0800

    [Car class] - create methods

--- car.py
+++ car.py
@@ -3,8 +3,25 @@
 # July 1, 2023
 
 class Car:
-    def __init__(self, year_model, make, speed):
+    def __init__(self, year_model, make, speed=0):
         self.__year_model = year_model
         self.__make = make
         self.__speed = speed
         
+    def accelerate (self):
+        speed = self.__speed + 5 
+        return speed
+    
+    def brake (self):
+        speed = self.__speed - 5
+        return speed
+    
+    def get_speed (self):
+        speed = self.__speed
+        return speed
+    
+car1 = Car(2020, "ewan", 60)
+
+print (car1.accelerate())
+print (car1.brake())
+print (car1.get_speed())
