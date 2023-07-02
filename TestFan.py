d8f1507a58ea6188db1e1790f483cb1d541aef35
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-02 15:27:54 +0800

    [Fan class] - Display fan 2 details

--- TestFan.py
+++ TestFan.py
@@ -1,10 +1,21 @@
 from fan import Fan
 
+# Create Objects
 fan1 = Fan (3, "on", 10, "yellow")
 fan2 = Fan (2, "off", 5, "blue")
 
-print ("Fan 1 Details\n")
+# Display output
+
+# Fan 1 Details
+print ("Fan 1\n")
 print ("Speed: ", fan1.get_speed())
 print ("Radius: ", fan1.get_radius())
 print ("On: ", fan1.get_on())
 print ("Color: ", fan1.get_color())
+
+# Fan 2 Details
+print ("\nFan 2\n")
+print ("Speed: ", fan2.get_speed())
+print ("Radius: ", fan2.get_radius())
+print ("On: ", fan2.get_on())
+print ("Color: ", fan2.get_color())
--- fan.py
+++ fan.py
@@ -49,8 +49,3 @@ class Fan:
         return self.__color
     
 
-        
-
-# Create objects
-# Display output
-
