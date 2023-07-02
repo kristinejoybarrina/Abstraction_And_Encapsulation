3442e7a1da8abb141517c9459d11ac73d167a7be
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-02 13:31:56 +0800

    [Pet class] - ask for input

---
+++ pet_object.py
@@ -0,0 +1,8 @@
+from pet import Pet
+
+# Let the user input their pet's name, type, and age
+print ("Introduce your pet!\n")
+name = input(str("Pet's name: "))
+type = input(str("Type of pet: "))
+age = input(str("Pet's age: "))
+
