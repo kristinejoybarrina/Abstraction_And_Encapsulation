4ae8e486f0da5afa0cec358103ead45909c48383
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-02 14:20:43 +0800

    [Fan class] - Pseudocode

---
+++ fan.py
@@ -0,0 +1,13 @@
+# Kristine Joy Barrina  # BSCPE 1-5
+# Abstraction and Encapsulation
+# July 1, 2023
+
+# Pseudocode
+
+# Create a class
+# Create 3 constants
+# Create constructor
+# Create methods
+# Create objects
+# Display output
+
