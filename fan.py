62a63009ef6467e2bd453fb47249ed0f4bd10e02
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-02 14:32:33 +0800

    [Fan class] - Create class, constants, and constructor

--- fan.py
+++ fan.py
@@ -5,8 +5,20 @@
 # Pseudocode
 
 # Create a class
+class Fan:
+
 # Create 3 constants
+    SLOW = 1
+    MEDIUM = 2
+    FAN = 3
+
 # Create constructor
+    def __init__(self, speed = "slow", on= "False", radius= 5, color="blue"):
+        self.__speed = int(speed)
+        self.__on = bool(on)
+        self.__radius = float(radius)
+        self.__color = str(color)
+
 # Create methods
 # Create objects
 # Display output
